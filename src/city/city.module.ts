92bf0403dd41486c9f938e2d1571186b0360b50f
Pedro Lara <pedroalveslara@gmail.com>
2023-04-13 15:53:31 -0300

    Create DTO using Pipe

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
 import { CityService } from './city.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CityEntity } from './entities/city.entity';
 import { CityController } from './city.controller';
+import { CacheModule } from 'src/cache/cache.module';
 
 @Module({
-  imports: [
-    CacheModule.register({
-      ttl: 9000000,
-    }),
-    TypeOrmModule.forFeature([CityEntity]),
-  ],
+  imports: [TypeOrmModule.forFeature([CityEntity]), CacheModule],
   controllers: [CityController],
   providers: [CityService],
 })
