72860f7fcb43afc0818526d88494a050433510fa
Pedro Lara <pedroalveslara@gmail.com>
2023-04-13 17:25:51 -0300

    Insert ID's validate on services

--- src/adress/adress.module.ts
+++ src/adress/adress.module.ts
@@ -3,9 +3,11 @@ import { AdressController } from './adress.controller';
 import { AdressService } from './adress.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdressEntity } from './entities/adress.entity';
+import { UserModule } from 'src/user/user.module';
+import { CityModule } from 'src/city/city.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AdressEntity])],
+  imports: [TypeOrmModule.forFeature([AdressEntity]), UserModule, CityModule],
   controllers: [AdressController],
   providers: [AdressService],
 })
--- src/adress/adress.service.ts
+++ src/adress/adress.service.ts
@@ -3,18 +3,26 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AdressEntity } from './entities/adress.entity';
 import { Repository } from 'typeorm';
 import { CreateAdressDto } from './dtos/createAdress.dto';
+import { UserService } from 'src/user/user.service';
+import { CityService } from 'src/city/city.service';
 
 @Injectable()
 export class AdressService {
   constructor(
     @InjectRepository(AdressEntity)
     private readonly adressRepository: Repository<AdressEntity>,
+    private readonly userService: UserService,
+    private readonly cityService: CityService,
   ) {}
 
   async createAdress(
     createAdressdTO: CreateAdressDto,
     userId: number,
   ): Promise<AdressEntity> {
+    await this.userService.findUserById(userId);
+
+    await this.cityService.findCityById(createAdressdTO.cityId);
+
     return this.adressRepository.save({
       ...createAdressdTO,
       userId,
