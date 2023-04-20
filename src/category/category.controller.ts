1a187fdffdc92e36c37af29e698050f60223a22e
Pedro Lara <pedroalveslara@gmail.com>
2023-04-20 09:17:20 -0300

    Category return format

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { CategoryEntity } from './entities/category.entity';
 import { CategoryService } from './category.service';
+import { ReturnCategory } from './dtos/returnCategory.dto';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Get()
-  async getAllCategories(): Promise<CategoryEntity[]> {
-    return await this.categoryService.findAllCategories();
+  async getAllCategories(): Promise<ReturnCategory[]> {
+    return (await this.categoryService.findAllCategories()).map(
+      (category) => new ReturnCategory(category),
+    );
   }
 }
