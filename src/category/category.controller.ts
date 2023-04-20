03932b021515d416ccfd7bec7fc3a1f9ac47abc6
Pedro Lara <pedroalveslara@gmail.com>
2023-04-20 09:38:15 -0300

    Update category.controller.ts

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { ReturnCategory } from './dtos/returnCategory.dto';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/user-type.enum';
 
+@Roles(UserType.Admin, UserType.User)
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
